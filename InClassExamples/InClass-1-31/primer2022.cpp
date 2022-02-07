5f6ab5d1be7448e9f7df39a39f4f8ed0fb163c49
sarah <sarah.ernst@marquette.edu>
2022-02-07 10:09:18 -0600

    adding edits from the weekend

--- InClassExamples/InClass-1-31/primer2022.cpp
+++ InClassExamples/InClass-1-31/primer2022.cpp
@@ -7,6 +7,23 @@
     }
 */
 
+//assuming you defined your constructor you need to do this
+// the point of a constructor is to construct your object
+// it's an initialization process - sometimes it doesn't have any data structures related to it
+// constructor is a function named the same thing as a class
+
+mySpace::yourSpace::myClass::myClass(std::string name, int init_num_students){
+    //if you want to initialize classname and num students
+    //this is where you set class variables up
+
+    this->className = name;
+    this->num_students = init_num_students;
+}
+
+int mySpace::yourSpace::myClass::get_num_students(){
+    return this->num_students;
+}
+
 //follow cascade of indentations for address of things you want to define from hpp
 int mySpace::yourSpace::myClass::get_num_students(){ 
     return this->num_students; //you can use keyword this -- everything within these brackets, assumes you're at this address
--- InClassExamples/InClass-1-31/primer2022.hpp
+++ InClassExamples/InClass-1-31/primer2022.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 //namespaces allow you to provide context necessary to track different things
 //give you address
@@ -20,14 +21,22 @@ namespace mySpace{
                 //construct myclass with an initial name and number of students
                 myClass(std::string name, int init_num_students); 
                 myClass(std::string name);
+                myClass();
+                ~myClass(); //deconstructor
 
                 int get_num_students();
                 std::string get_name();
                 void set_num_students(int num);
+                void set_name(str::string newname);
+                std::vector<std::string>* get_classlsit();
 
             private:
                 int num_students;
                 std::string className;
+                // about vectors: 
+                // vectors are resizable arrays
+                // they don't need to reallocate space as the list grows
+                std::vector<std::string> classlist; //list of student names
         };
     }
 }
